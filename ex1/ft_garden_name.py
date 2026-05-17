b7abd34eb6f9f08cb8c5dcf2d1882af7a9a3538e
Takaho Takemura <ttakemur@c1r6s6.42tokyo.jp>
2026-05-18 04:39:48 +0900

    init

---
+++ ex1/ft_garden_name.py
@@ -0,0 +1,6 @@
+def ft_garden_name():
+    printf("Enter garden name: ")
+    garden_name = input
+    printf("Garden: %s", )Community Garden
+    printf("Status: Growing well!")
+
---
+++ test.py
@@ -0,0 +1,19 @@
+from ex0.ft_hello_garden import ft_hello_garden
+# from ex1.ft_garden_name import ft_garden_name
+# from ex2.ft_plot_area import ft_plot_area
+# from ex3.ft_harvest_total import ft_harvest_total
+# from ex4.ft_plant_age import ft_plant_age
+# from ex5.ft_water_reminder import ft_water_reminder
+# from ex6.ft_count_harvest_iterative import ft_count_harvest_iterative  
+# from ex6.ft_count_harvest_recursive import ft_count_harvest_recursive
+# from ex7.ft_seed_inventory import ft_seed_inventory
+
+ft_hello_garden()
+# ft_garden_name()
+# ft_plot_area()
+# ft_harvest_total()
+# ft_plant_age()
+# ft_water_reminder()
+# ft_count_harvest_iterative()
+# ft_count_harvest_recursive()
+# ft_seed_inventory()
